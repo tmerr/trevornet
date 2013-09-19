71f223d5c6533298b1e25b9a02eba4e3a442abf3
Trevor Merrifield <trevorm42@gmail.com>
2013-09-18 23:55:49 -0400

    Refactor neurons.py, begin refactoring nets.py
    Neuron classes are much better now.
    
    It's annoying to access neurons through the Layer classes, so I
    considered removing them. Now that I'm 90% of the way there I realize
    it's a mistake. Accessing neurons through indexes is just as awkward.
    I now believe the best solution is to make Layer act like a collection
    where I can iterate over it etc.

--- TrevorNet/nets.py
+++ TrevorNet/nets.py
@@ -1,81 +1,84 @@
-from layers import InputLayer, HiddenLayer, OutputLayer
-import math
-import random
+from neurons import InputNeuron, HiddenNeuron, OutputNeuron
 
 class FeedForwardNet(object):
-    def __init__(self, inlayersize, layersize, outlayersize):
-        self._inlayer = InputLayer(inlayersize)
-        self._hiddenlayer = HiddenLayer(layersize)
-        self._outlayer = OutputLayer(outlayersize)
+    """A backpropagating feed forward neural network."""
 
-        self._inlayer.connect_layer(self._hiddenlayer)
-        self._hiddenlayer.connect_layer(self._outlayer)
+    def __init__(self, *layers):
+        """Parameters are the neuron count for each layer. For example to make a
+        network with 2 input neurons, 2 hidden neurons, and 1 output neuron
+        you would pass (2, 2, 1). A four layer network could be constructed with
+        (1, 3, 3, 7)."""
+        if len(layers) < 2:
+            raise ValueError("Need at least two layers")
+
+        #create layers of neurons (which can also be accessed via properties)
+        self._layers = [
+            [InputNeuron() for i in range(layers[0])],
+            [[HiddenNeuron() for j in range(layers[k])]
+            for k in range(1, len(layers)-1)],
+            [OutputNeuron() for l in range(layers[-1])]
+        ]
+
+        #connect layers
+        for i in range(len(self._layers)-1):
+            for pair in itertools.product(self._layers[i], self._layers[i+1]):
+                pair[0].connect_forward(pair[1], 1)
+
+    def train(self, data, targets):
+        """Train the neural net with the given input data and targets.
+
+        Params:
+            data: Some sequence of input data to map to input neurons
+            targets: Some sequence of output data to compare against output neurons
+        """
+
+        if len(data) != len(self.inputlayer):
+            raise ValueError("Data must have same number of elements as input layer")
+        elif len(targets) != len(self.outputlayer):
+            raise ValueError("Targets must have same number of elements as output layer")
+
+        #propagate
+        self._propagate(data)
+        
+        #backpropagate
+        for idx, val in enumerate(targets):
+            self.outputlayer[idx].backpropagate1(val)
+        for layer in self.hiddenlayers[::-1]:
+            for neuron in layer:
+                neuron.backpropagate()
+
+    def predict(self, data):
+        """Predict the target for the data.
+        
+        Params:
+            data: Some sequence of input data to map to input neurons
+
+        Return:
+            a sequence of numerical signals from the output neurons.
+        """
+        if len(data) != len(self.inputlayer):
+            raise ValueError("Data must have same number of elements as input layer")
+
+        self._propagate(data)
+        out = [neuron.signal for neuron in self.outputlayer]
+        return out
+
+    def _propagate(self, data):
+        for idx, val in enumerate(data):
+            self.inputlayer[idx] = val
+        for layer in self._layers[:-1]:
+            for neuron in layer:
+                neuron.propagate()
 
     @property
-    def neurons(self):
-        return [self._inlayer.neurons, self._hiddenlayer.neurons, self._outlayer.neurons]
+    def inputlayer(self):
+        return self._layers[0]
 
-    def train(self, inputs, targets, verbose=False):
-        '''
-        inputs: a sequence of floats that map to the input neurons
-        targetlabels: a sequence of floats that are the desired output neuron
-                      values.
-        '''
+    @property
+    def hiddenlayers(self):
+        return self._layers[1:-1]
 
-        self._inlayer.inputs = inputs
-        self._hiddenlayer.propagate()
-        self._outlayer.propagate()
-        
-        self._outlayer.backpropagate1(targets)
-        self._hiddenlayer.backpropagate1()
+    @property
+    def outputlayer(self):
+        return self._layers[-1]
 
-        self._outlayer.backpropagate2()
-        self._hiddenlayer.backpropagate2()
-
-        if verbose:
-            print("Training results")
-            print("\tInput: {0}".format(inputs))
-            print("\tTarget output: {0}".format(targets))
-            print("\tActual output: {0}".format(self._outlayer.outputs))
-            self.display_signals()
-            self.display_weights_from(0, 0)
-            print("")
-
-    def predict(self, inputs):
-        '''
-        inputs: a sequence of floats that map to the input neurons
-        return: a sequence of floats mapped from the output neurons
-        '''
-        self._inlayer.inputs = inputs
-        self._hiddenlayer.propagate()
-        self._outlayer.propagate()
-        return self._outlayer.outputs
-
-    def display_signals(self):
-        col1 = self._inlayer.inputs
-        col2 = [x.signal for x in self._hiddenlayer.neurons]
-        col3 = self._outlayer.outputs
-        numrows = max(len(col1), len(col2), len(col3))
-
-        roundto = 3 #round to
-        print("Signals")
-        print("\tInput\tHidden\tOutput")
-        for row in range(numrows):
-            line = []
-            for col in col1, col2, col3:
-                if len(col)-1 < row:
-                    line.append("")
-                else:
-                    element = round(col[row], roundto)
-                    element = str(element)
-                    line.append(element)
-            print('\t' + '\t'.join(line))
-
-    def display_weights_from(self, layer, neuron):
-        l = (self._inlayer, self._hiddenlayer, self._outlayer)[layer]
-        n = l.neurons[neuron]
-        out = []
-        for i, conn in enumerate(n._forward):
-            out.append('[{0}]={1}'.format(i, conn.weight))
-        print(' '.join(out))
-        print('')
--- TrevorNet/neurons.py
+++ TrevorNet/neurons.py
@@ -2,10 +2,54 @@ import math
 import aimath
 import random
 
+class PropagatingNeuron(object):
+    def __init__(self, learningrate):
+        self._forward = []
+        self._back = []
+        self._signal = 0
+        self._errsignal = 0
+        self._learningrate = learningrate
+
+    def connect_forward(self, other, weight):
+        conn = Connection(self, other, weight)
+        self._forward.append(conn)
+        other._back.append(conn)
+
+    @property
+    def signal(self):
+        return self._signal
+
+    @property
+    def errsignal(self):
+        return self._errsignal
+
+    def propagate(self):
+        thesum = sum([b.weight * b.signal for b in self._back])
+        self._signal = aimath.sigmoid(thesum + self._bias)
+
+    def backpropagate1(self):
+        raise NotImplementedError()
+
+    def backpropagate2(self):
+        z = self._learningrate * self._errsignal
+        for b in self._back:
+            b.weight -= z * b.signal
+
+        self._bias -= self._learningrate * self._errsignal
+
+class OutputNeuron(PropagatingNeuron):
+    def backpropagate1(self, target):
+        s = self.signal
+        self._neuron._errsignal = aimath.sigmoidprime(s) * (s - target)
+
+class HiddenNeuron(PropagatingNeuron):
+    def backpropagate1(self):
+        errsum = sum([f.weight * f.errsignal for f in self._forward])
+        self._errsignal = aimath.sigmoidprime(self._signal) * errsum
+
 class InputNeuron(object):
     def __init__(self):
         self._forward = []
-        self._back = []
         self._signal = 0
 
     @property
@@ -16,74 +60,10 @@ class InputNeuron(object):
     def signal(self, value):
         self._signal = value
 
-    def attach_forward(self, connection):
-        self._forward.append(connection)
-
-class OutputNeuron(object):
-    def __init__(self, learningrate):
-        self._neuron = Neuron(learningrate)
-
-    @property
-    def signal(self):
-        return self._neuron._signal
-
-    @property
-    def errsignal(self):
-        return self._neuron._errsignal
-
-    def attach_back(self, connection):
-        self._neuron.attach_back(connection)
-
-    def propagate(self):
-        self._neuron.propagate()
-
-    def backpropagate1(self, target):
-        '''Update error signal based on the given target value'''
-        s = self.signal
-        self._neuron._errsignal = aimath.sigmoidprime(s) * (s - target)
- 
-    def backpropagate2(self):
-        self._neuron.backpropagate2()
-
-class Neuron(object):
-    def __init__(self, learningrate=.2):
-        self._forward = []
-        self._back = []
-        self._signal = 0
-        self._errsignal = 0
-        self._learningrate = learningrate
-        self._bias = 1
-
-    @property
-    def signal(self):
-        return self._signal
-
-    @property
-    def errsignal(self):
-        return self._errsignal
-
-    def attach_forward(self, connection):
-        self._forward.append(connection)
-
-    def attach_back(self, connection):
-        self._back.append(connection)
-
-    def propagate(self):
-        thesum = sum([b.weight * b.signal for b in self._back])
-        self._signal = aimath.sigmoid(thesum + self._bias)
-
-    def backpropagate1(self):
-        '''Update error signal'''
-        errsum = sum([f.weight * f.errsignal for f in self._forward])
-        self._errsignal = aimath.sigmoidprime(self._signal) * errsum
-
-    def backpropagate2(self):
-        '''Adjust weights and bias'''
-        z = self._learningrate * self._errsignal
-        for b in self._back:
-            b.weight -= z * b.signal
-
-        self._bias -= self._learningrate * self._errsignal
+    def connect_forward(self, other, weight):
+        conn = Connection(self, other, weight)
+        self._forward.append(conn)
+        other._back.append(conn)
 
 class Connection(object):
     def __init__(self, back, forward, weight):
