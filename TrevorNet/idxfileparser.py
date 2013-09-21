50f44ddb133395a8822435e19ffd68a6abd26c6f
Trevor Merrifield <trevorm42@gmail.com>
2013-09-20 22:18:48 -0400

    Add idxfileparser.py

---
+++ TrevorNet/idxfileparser.py
@@ -0,0 +1,6 @@
+"""Parse an IDX file like the one used in the MNIST handwritten digit database"""
+
+def idx_to_list(fname):
+    """Parse the IDX file and return an n dimensional list."""
+    with open(fname) as f:
+        pass
