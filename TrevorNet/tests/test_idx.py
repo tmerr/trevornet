12f835d9060decfc675c81f7a1499b373b78f4cc
Trevor Merrifield <trevorm42@gmail.com>
2013-09-21 16:32:03 -0400

    Fix issue where idx test uses wrong bytes object
    Forgot to include the sizes of each dimension

--- TrevorNet/tests/test_idx.py
+++ TrevorNet/tests/test_idx.py
@@ -12,10 +12,17 @@ def check__count_dimensions(lst, i):
 
 # these two are equivalent according to the format on http://yann.lecun.com/exdb/mnist/
 _somelist = [[1, 2], [3, 4]]
-_somebytes = b'\x00\x00\x0C\x02' + b'\x01\x02\x03\x04'
+def _get_somebytes():
+    header = b'\x00\x00\x0C\x02'
+    dimensionsizes = b'\x00\x00\x00\x02' + b'\x00\x00\x00\x02'
+    data = b'\x00\x00\x00\x01' + b'\x00\x00\x00\x02'
+    data += b'\x00\x00\x00\x03' + b'\x00\x00\x00\x04'
+    return header + dimensionsizes + data
+_somebytes = _get_somebytes()
 
 def test_list_to_idx():
     data = idx.list_to_idx(_somelist, 'i')
+    print(data, _somebytes)
     assert data == _somebytes
 
 def test_idx_to_list():
