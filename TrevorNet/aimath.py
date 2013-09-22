1170a9767bf9a16735375bbc90c31ae6cbc80492
Trevor Merrifield <trevorm42@gmail.com>
2013-09-22 17:51:46 -0400

    Fix overflow bug
    Sigmoid function can no longer overflow

--- TrevorNet/aimath.py
+++ TrevorNet/aimath.py
@@ -1,7 +1,12 @@
 import math
+import sys
 
 def sigmoid(x):
-    return 1/(1 + math.exp(-x))
+    try:
+        val = 1/(1 + math.exp(-x))
+    except OverflowError:
+        val = sys.float_info.max
+    return val
 
 def sigmoidprime(x):
     return (1 - sigmoid(x))*sigmoid(x)
